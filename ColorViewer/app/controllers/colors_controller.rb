f43627c5a2483df7165ecc49c23a84ae195ffd98
hkliya <hkliya@gmail.com>
2014-05-25 08:33:35 +0800

    add buttons

--- ColorViewer/app/controllers/colors_controller.rb
+++ ColorViewer/app/controllers/colors_controller.rb
@@ -11,5 +11,20 @@ class ColorsController < UIViewController
     @label.center = [self.view.size.width / 2, self.view.size.height / 2]
     @label.autoresizingMask = UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleTopMargin
     self.view.addSubview(@label)
+
+    ["red", "yellow", "blue"].each_with_index do |color_text, index|
+      color = UIColor.send("#{color_text}Color")
+      button_width = 80
+
+      button = UIButton.buttonWithType(UIButtonTypeRoundedRect)
+      button.setTitle(color_text, forState: UIControlStateNormal)
+      button.setTitleColor(color, forState: UIControlStateNormal)
+      button.sizeToFit
+      button.frame = [[30 + index * (button_width + 10), @label.frame.origin.y + button.frame.size.height + 10], [button_width, button.frame.size.height]]
+      button.autoresizingMask = UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleTopMargin
+      button.addTarget(self, action: "#{color_text}_tapped", forControlEvents: UIControlEventTouchUpInside)
+
+      self.view.addSubview(button)
+    end
   end
 end
